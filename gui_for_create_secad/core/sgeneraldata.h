3c130739bcb145b1e6ccb5221df93dfacb61fb2b
baseoleph <baseoleph@gmail.com>
2020-11-08 13:13:58 +0300

    declaring variables for SGeneralData

--- gui_for_create_secad/core/sgeneraldata.h
+++ gui_for_create_secad/core/sgeneraldata.h
@@ -7,40 +7,64 @@ public:
     SGeneralData();    
     ~SGeneralData();
 
-    //Длина, м
+    // Длина
+    // Leight
+    // meters
     double leight;
 
-    //
+    // Ширина, м
+    // Beam
+    // meters
     double deam;
 
-    //
+    // Осадка, м
+    // Depths
+    // meters
     double depths;
 
-    //
+    // Высота борта, м
+    // Freeboard
+    // meters
     double height;
 
+    // Коэффициент общей полноты
+    // Block Coefficient
     //
     double cb;
 
+    // Коэффициент развитости надстроек
+    // Superstructure Development Coefficient
     //
     double csd;
 
-    //
+    // Поперечная метацентрическая высота (МЦВ), м
+    // Metacentric Height
+    // meters
     double gm;
 
-    //
-    double wind_pressure;
+    // Давление ветра, Па
+    // Wind Pressure
+    // pascals
+    double wind_pressure = 1200;
 
-    //
-    double wha;
+    // Максимально допустимый угол крена от действия ветра, градусы
+    // Wind Heel Angle
+    // degrees
+    double wha = 15;
 
-    //
+    // Заданное значение максимальной аппликаты огибающей силуэта, м
+    // SEFm - Silhouette Envelope Function max
+    // meters
     double sef_max;
 
-    //
+    // Относительная абсцисса максимума огибающей силуэта (доли длины)
+    // ROSEFm - SEFm relative ordinate
+    // proportion
     double rosef_max;
 
-    //
+    // Относительная аппликата огибающей силуэта на кормовом перпендикуляре (доля SEFm)
+    // APRA - Aft Perpendicular Relative Applicate
+    // proportion
     double apra;
 
 };
