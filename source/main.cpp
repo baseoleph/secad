ab4bd61eff0a73aea9f5864ba201356aa234a4bf
baseoleph <baseoleph@gmail.com>
2021-01-17 01:58:39 +0300

    add license

--- source/main.cpp
+++ source/main.cpp
@@ -1,13 +1,23 @@
+// Ship Exterior CAD
+// Copyright (C) 2020 baseoleph@gmail.com,
+//					  github.com/RoninHikami
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #include "gui/mainwindow.h"
 
 #include <QApplication>
-#include <iomanip>
-
-//// Умный указатель на файл логирования
-//QScopedPointer<QFile>   m_logFile;
-
-//// Объявляение обработчика
-//void messageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg);
 
 int main(int argc, char *argv[])
 {
