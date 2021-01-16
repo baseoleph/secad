0edad0b49a61d682d2d66ba299cb88ab4a62d39f
baseoleph <baseoleph@gmail.com>
2021-01-17 01:59:02 +0300

    minor clean

--- source/main.cpp
+++ source/main.cpp
@@ -22,36 +22,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-////     Устанавливаем файл логирования,
-////     внимательно сверьтесь с тем, какой используете путь для файла
-//    m_logFile.reset(new QFile("logFile.txt"));
-//    // Открываем файл логирования
-//    m_logFile.data()->open(QFile::Append | QFile::Text);
-//    // Устанавливаем обработчик
-//    qInstallMessageHandler(messageHandler);
     MainWindow w;
     w.show();
 
     return a.exec();
 }
-
-//// Реализация обработчика
-//void messageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
-//{
-//    Q_UNUSED(type)
-//    if (QString(context.category) == "info")
-//    {
-//        // Открываем поток записи в файл
-//        QTextStream out(m_logFile.data());
-//        // Записываем дату записи
-//        out << QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss.zzz ");
-//        // Записываем в вывод категорию сообщения и само сообщение
-//        out << ": " << msg << Qt::endl;
-//        out.flush();    // Очищаем буферизированные данные
-//    }
-//    else
-//    {
-//        qDebug() << msg;
-//    }
-//}
